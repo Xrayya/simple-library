c96e1fec443c715a32c2d3fef65fcde51a57df2e
Xrayya <nur.alkhiyar@gmail.com>
2025-08-07 18:51:26 +0700

    fix(fe.field-info): unparsed message

--- frontend/src/components/field-info.tsx
+++ frontend/src/components/field-info.tsx
@@ -6,9 +6,22 @@ export function FieldInfo({ field }: { field: AnyFieldApi }) {
   const isValidating = field.state.meta.isValidating;
 
   return (
-    <div className="min-h-[1rem] text-xs font-extralight mt-1">
+    <div className="min-h-[1rem] text-xs mt-1">
       {showError && (
-        <p className="text-destructive">{field.state.meta.errors.join(", ")}</p>
+        <p className="text-destructive">
+          {field.state.meta.errors 
+            .map((error) => {
+              if (
+                typeof error === "object" &&
+                typeof error.message === "string"
+              ) {
+                return error.message;
+              }
+
+              return error;
+            })
+            .join(", ")}
+        </p>
       )}
       {isValidating && (
         <p className={cn("text-muted-foreground", showError && "mt-1")}>

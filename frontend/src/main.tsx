8ae1e761e6a187e23a512df66ae1d7da15b87a75
Xrayya <nur.alkhiyar@gmail.com>
2025-08-07 18:40:11 +0700

    feat(fe): theme provider and toggle

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
-import "./styles.css";
-import reportWebVitals from "./reportWebVitals.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import reportWebVitals from "./reportWebVitals.ts";
+import "./styles.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 const queryClient = new QueryClient();
 
@@ -35,7 +36,9 @@ if (rootElement && !rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <ThemeProvider>
+          <RouterProvider router={router} />
+        </ThemeProvider>
       </QueryClientProvider>
     </StrictMode>,
   );

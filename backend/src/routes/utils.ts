09570052182bed010cc29d72402db5f2c39ef648
Xrayya <nur.alkhiyar@gmail.com>
2025-08-08 10:57:35 +0700

    chore(be.utils): add check cookie endpoint

--- backend/src/routes/utils.ts
+++ backend/src/routes/utils.ts
@@ -7,13 +7,52 @@ import {
   refreshTokens,
   users,
 } from "../db/schema";
+import { getCookie, setCookie } from "hono/cookie";
 
-export const utilsRoute = new Hono().get("/clear-db", async (c) => {
-  await db().delete(borrowLogs);
-  await db().delete(refreshTokens);
-  await db().delete(users);
-  await db().delete(books);
-  await db().delete(categories);
+export const utilsRoute = new Hono()
+  .get("/clear-db", async (c) => {
+    await db().delete(borrowLogs);
+    await db().delete(refreshTokens);
+    await db().delete(users);
+    await db().delete(books);
+    await db().delete(categories);
 
-  return c.json({ message: "Database cleared successfully" }, 200);
-});
+    return c.json({ message: "Database cleared successfully" }, 200);
+  })
+  .post("/clear-token-cookies", async (c) => {
+    setCookie(c, "accessToken", "", {
+      httpOnly: true,
+      // secure: true,
+      sameSite: "Lax",
+      maxAge: 0,
+      path: "/",
+    });
+
+    setCookie(c, "refreshToken", "", {
+      httpOnly: true,
+      // secure: true,
+      sameSite: "Lax",
+      maxAge: 0,
+      path: "/",
+    });
+
+    return c.json({ message: "Token cookie cleaning respons sent" }, 200);
+  })
+  .post("/get-cookies", async (c) => {
+    const accessToken =
+      getCookie(c, "accessToken") ||
+      c.req.header("Authorization")?.split(" ")[1];
+
+    const refreshToken = getCookie(c, "refreshToken");
+
+    console.log("Access Token:", accessToken);
+    console.log("Refresh Token:", refreshToken);
+
+    return c.json(
+      {
+        accessToken: accessToken || "empty",
+        refreshToken: refreshToken || "empty",
+      },
+      200,
+    );
+  });

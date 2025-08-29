b8b2c8f203c746e0020997d1f0101fc999eec414
Xrayya <nur.alkhiyar@gmail.com>
2025-08-29 09:24:13 +0700

    feat(fe.hooks): add auth param and auto fetch refresh token and refetch previous request on 401

--- frontend/src/hooks/useApi.ts
+++ frontend/src/hooks/useApi.ts
@@ -7,18 +7,41 @@ import {
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
-async function fetcher<T>(url: string, options?: RequestInit): Promise<T> {
-  const response = await fetch(BASE_URL + url, {
+async function fetcher<T>(
+  url: string,
+  options?: RequestInit,
+  auth?: boolean,
+): Promise<T> {
+  let response = await fetch(BASE_URL + url, {
     headers: { "Content-Type": "application/json" },
+    credentials: auth ? "include" : undefined,
     ...options,
   });
 
+  if (response.status === 401 && auth) {
+    const payload: any = await response.json();
+    console.error("Unauthorized access - 401", payload);
+    await fetch(BASE_URL + "/auth/refresh", {
+      credentials: "include",
+    });
+
+    response = await fetch(BASE_URL + url, {
+      headers: { "Content-Type": "application/json" },
+      credentials: auth ? "include" : undefined,
+      ...options,
+    });
+  }
+
   if (!response.ok) {
-    const payload: any = await response.json().catch(() => ({}));
+    const payload: any = await response.json();
     console.error("Error fetching data:", payload);
     throw new Error(
       payload?.error?.message || "An error occurred while fetching data",
     );
+  }
+
+  if (response.status === 204) {
+    return {} as T;
   }
 
   return response.json();
@@ -27,11 +50,15 @@ async function fetcher<T>(url: string, options?: RequestInit): Promise<T> {
 export function useApiQuery<T = unknown>(
   queryKey: string[],
   url: string,
-  options?: { query?: UseQueryOptions<T>; fetcher?: RequestInit },
+  options?: {
+    query?: UseQueryOptions<T>;
+    fetcher?: RequestInit;
+  },
+  auth?: boolean,
 ) {
   return useQuery<T>({
     queryKey,
-    queryFn: () => fetcher<T>(url, options?.fetcher),
+    queryFn: () => fetcher<T>(url, options?.fetcher, auth),
     ...options?.query,
   });
 }
@@ -43,14 +70,19 @@ export function useApiMutation<TInput = unknown, TOutput = unknown>(
     mutation?: UseMutationOptions<TOutput, Error, TInput>;
     fetcher?: RequestInit;
   },
+  auth?: boolean,
 ) {
   return useMutation<TOutput, Error, TInput>({
     mutationFn: (data: TInput) =>
-      fetcher<TOutput>(url, {
-        method,
-        body: JSON.stringify(data),
-        ...options?.fetcher,
-      }),
+      fetcher<TOutput>(
+        url,
+        {
+          method,
+          body: JSON.stringify(data),
+          ...options?.fetcher,
+        },
+        auth,
+      ),
     ...options?.mutation,
   });
 }

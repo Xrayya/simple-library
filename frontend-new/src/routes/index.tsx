883ec93140dd3b7913f1163381f9f52b72f64497
Xrayya <nur.alkhiyar@gmail.com>
2026-08-18 15:04:20 +0700

    fix(frontend-new): remove import logo.svg since it's nonexistent

--- frontend-new/src/routes/index.tsx
+++ frontend-new/src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import logo from "../logo.svg";
 
 export const Route = createFileRoute("/")({ component: Home });
 
@@ -7,11 +6,6 @@ function Home() {
   return (
     <div className="text-center">
       <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-[calc(10px+2vmin)] text-white">
-        <img
-          src={logo}
-          className="pointer-events-none h-[40vmin] animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
         <p>
           Edit <code>src/routes/index.tsx</code> and save to reload.
         </p>

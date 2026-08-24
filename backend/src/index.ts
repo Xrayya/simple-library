c86439a6fc6a9a23cbbbcb605ae9f299b25e38a6
Xrayya <nur.alkhiyar@gmail.com>
2026-08-24 09:48:05 +0700

    feat: import from simple note taking app project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,21 +2,40 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { HTTPException } from "hono/http-exception";
 import { logger } from "hono/logger";
+import { IS_PROD, env } from "./env";
 import { BaseError } from "./exceptions/base";
+import { InvalidRequestError } from "./exceptions/validation";
 import { authRoute } from "./routes/auth";
 import { booksRoute } from "./routes/books";
 import { utilsRoute } from "./routes/utils";
 
-const backend = new Hono().basePath("/api/v1");
+console.log("Runtime Check:", {
+  IS_PROD,
+  NODE_ENV: env.NODE_ENV,
+  CLIENT_ORIGIN: env.CLIENT_ORIGIN,
+});
+
+const backend = new Hono();
+
+backend.use(logger());
 
 backend.use(
   "*",
   cors({
-    origin: ["http://localhost:3000"],
+    origin: (origin) => {
+      if (!origin) return !IS_PROD ? "*" : null;
+
+      if (env.CLIENT_ORIGIN === origin) {
+        return origin;
+      }
+
+      return null;
+    },
     credentials: true,
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
   }),
 );
-backend.use(logger());
 
 backend.get("/", (c) => {
   return c.json({ message: "You are in root" });
@@ -33,6 +52,15 @@ backend.onError((err, c) => {
     return c.json(
       { error: { name: err.name, message: err.message } },
       err.status,
+    );
+  }
+
+  if (err instanceof InvalidRequestError) {
+    console.log("traces", err.traces);
+
+    return c.json(
+      { error: { name: err.name, message: err.message, traces: err.traces } },
+      err.statusCode,
     );
   }
 
